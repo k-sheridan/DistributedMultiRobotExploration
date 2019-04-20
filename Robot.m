1d1e19a973daa370ac0fa7e6d3f72a15cd8df570
Trexter <sheridak@purdue.edu>
2019-04-19 20:53:56 -0400

    setting up the robot

--- Robot.m
+++ Robot.m
@@ -9,6 +9,12 @@ classdef Robot
         communicationRate = 100; % Quadtree nodes per second
         
         forwardVelocity = 0.5; % m/s
+        
+        state; % stores the robot's estimate of its position in the 'global frame'
+        
+        localMap; % a local occupancy grid the robot will update as it explores.
+        
+        explorationTree; % a quad tree used to efficiently communicate map exploration status.
     end
     
     methods
